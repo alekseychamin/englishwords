8b843c77c52f37206ae59834990d5dfe6a69afc9
Aleksei Chamin <aleksei_chamin@epam.com>
2021-11-19 21:16:11 +0300

    Initial migration

---
+++ EnglishWordApi/Infrastructure/Migrations/20211117204514_IntialCreate.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class IntialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EnglishGroup",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EnglishGroup", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EnglishWord",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Phrase = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Transcription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Translation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Example = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PictureUri = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EnglishGroupId = table.Column<int>(type: "int", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EnglishWord", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EnglishWord_EnglishGroup_EnglishGroupId",
+                        column: x => x.EnglishGroupId,
+                        principalTable: "EnglishGroup",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EnglishWord_EnglishGroupId",
+                table: "EnglishWord",
+                column: "EnglishGroupId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EnglishWord");
+
+            migrationBuilder.DropTable(
+                name: "EnglishGroup");
+        }
+    }
+}
