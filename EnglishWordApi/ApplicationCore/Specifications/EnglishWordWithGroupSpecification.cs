7b53b1366556748f367eb2812a8258f664e510d4
Aleksei Chamin <aleksei_chamin@epam.com>
2021-11-21 15:02:19 +0300

    Update specs

---
+++ EnglishWordApi/ApplicationCore/Specifications/EnglishWordWithGroupSpecification.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationCore.Entities;
+using Ardalis.Specification;
+
+namespace ApplicationCore.Specifications
+{
+    public class EnglishWordWithGroupSpecification : Specification<EnglishWord>, ISingleResultSpecification
+    {
+        public EnglishWordWithGroupSpecification(int englishWordId)
+        {
+            Query
+                .Where(x => x.Id == englishWordId)
+                .Include(x => x.EnglishGroup);
+        }
+    }
+}
