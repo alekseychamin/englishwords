f70423f5f863d5d74ca44e34606992b6a23fc60f
Aleksei Chamin <aleksei_chamin@epam.com>
2021-11-24 15:38:51 +0300

    Added new specification

---
+++ src/ApplicationCore/Specifications/EnglishGroupWithFilter.cs
@@ -0,0 +1,28 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Specifications.Filter;
+using Ardalis.Specification;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Specifications
+{
+    public class EnglishGroupWithFilter : Specification<EnglishGroup>
+    {
+        public EnglishGroupWithFilter(EnglishGroupFilter filter)
+        {
+            if (!string.IsNullOrEmpty(filter.Name))
+            {
+                Query.Search(x => x.Name, "%" + filter.Name + "%");
+            }
+
+            if (filter.IsPagingEnabled)
+            {
+                Query.Skip(PaginationHelper.CalculateSkip(filter))
+                     .Take(PaginationHelper.CalculateTake(filter));
+            }
+
+            Query.OrderBy(x => x.Name);
+        }
+    }
+}
