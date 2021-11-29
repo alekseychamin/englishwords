997daa332f1bd5b124126b982b21b2c4140a8e48
Aleksei Chamin <aleksei_chamin@epam.com>
2021-11-29 23:53:07 +0300

    Added EnglishGroup unit tests

---
+++ UnitTests/ApplicationCore/Entitties/EnglishGroupTests.cs
@@ -0,0 +1,41 @@
+﻿using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests.ApplicationCore.Entitties
+{
+    public class EnglishGroupTests
+    {
+        [Fact]
+        public void EnglishGroupCreateThrowsArgumentNullExeption()
+        {
+            Assert.Throws<ArgumentNullException>(() => new EnglishGroup(null));
+        }
+
+        [Fact]
+        public void EnglishGroupCreateThrowsArgumentExeption()
+        {
+            Assert.Throws<ArgumentException>(() => new EnglishGroup(string.Empty));
+        }
+
+        [Fact]
+        public void EnglishGroupUpdateThrowsArgumentNullException()
+        {
+            var englishGroup = new EnglishGroup("Group1");
+
+            Assert.Throws<ArgumentNullException>(() => englishGroup.Update(null));
+        }
+
+        [Fact]
+        public void EnglishGroupUpdateThrowsArgumentException()
+        {
+            var englishGroup = new EnglishGroup("Group1");
+
+            Assert.Throws<ArgumentException>(() => englishGroup.Update(string.Empty));
+        }
+    }
+}
