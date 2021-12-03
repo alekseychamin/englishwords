a933bf1b1a2f58a181bb8b7cc40ca0cd954c2045
Aleksei Chamin <aleksei_chamin@epam.com>
2021-12-04 00:13:31 +0300

    Create helper class

---
+++ src/ApplicationCore/Specifications/FilterHelper.cs
@@ -0,0 +1,32 @@
+﻿using ApplicationCore.Entities;
+using Ardalis.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ApplicationCore.Specifications
+{
+    public enum SearchType
+    {
+        Like = 0
+    }
+
+    public static class FilterHelper
+    {
+        private static Dictionary<SearchType, Func<string, string>> processSearchTerms =
+            new Dictionary<SearchType, Func<string, string>>()
+            {
+                { SearchType.Like, (s) => { return $"%{s}%"; } }
+            };
+        
+        public static void SearchByTerms<T>(ISpecificationBuilder<T> spec, Expression<Func<T, string>> selector, 
+            string searchTerm, SearchType type) where T : BaseEntity
+        {
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                spec.Search<T>(selector, processSearchTerms[type](searchTerm));
+            }
+        }
+    }
+}
