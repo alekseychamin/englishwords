8666431253a2945682c3c044ef7a7ea3c0f5013f
Aleksei Chamin <aleksei_chamin@epam.com>
2022-01-04 20:47:52 +0300

    Add FunctionalTests

---
+++ FunctionalTests/PublicApi/ApiTestFixture.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using PublicAPI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.DataAccess;
+using Microsoft.Extensions.Logging;
+using ApplicationCore.Entities.Seeds;
+using System.IO;
+
+namespace FunctionalTests.PublicApi
+{
+    public class ApiTestFixture : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Testing");
+
+            builder.ConfigureServices(services => 
+            {
+                services.AddEntityFrameworkInMemoryDatabase();
+
+                // Create a new service provider.
+                var provider = services
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                // Add a database context (ApplicationDbContext) using an in-memory 
+                // database for testing.
+                services.AddDbContext<EnglishWordDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                    options.UseInternalServiceProvider(provider);
+                });
+
+                // Build the service provider.
+                var sp = services.BuildServiceProvider();
+
+                using var scope = sp.CreateScope();
+
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<EnglishWordDbContext>();
+                var loggerFactory = scopedServices.GetRequiredService<ILoggerFactory>();
+
+                var logger = scopedServices
+                    .GetRequiredService<ILogger<ApiTestFixture>>();
+
+                // Ensure the database is created.
+                db.Database.EnsureCreated();
+
+                try
+                {
+                    var seedJson = new SeedFromJsonEnglishWord(Path.Combine(AppContext.BaseDirectory, "englishwords.json"));
+
+                    new EnglishWordDbContextSeed(db).SeedAsync(seedJson).Wait();
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, $"An error occurred seeding the " +
+                            $"database with test messages. Error: {ex.Message}");
+                }
+            });
+        }
+    }
+}
---
+++ FunctionalTests/PublicApi/EnglishWordEndpoints/CreateEndpoint.cs
@@ -0,0 +1,47 @@
+﻿using ApplicationCore.Extensions;
+using PublicApi.Endpoints.EnglishWords;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FunctionalTests.PublicApi.EnglishWordEndpoints
+{
+    public class CreateEndpoint : IClassFixture<ApiTestFixture>
+    {
+        public HttpClient Client { get; }
+
+        public CreateEndpoint(ApiTestFixture factory)
+        {
+            Client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task ReturnSuccessCreatedNewEnglishWord()
+        {
+            // Arrange
+            var request = new CreateEnglishWordRequest()
+            {
+                Phrase = "Phrase 01",
+                Transcription = "Transcription 01",
+                Translation = "Translation 01",
+                Example = "Example 01"
+            };
+
+            StringContent content = new StringContent(request.ToJson(), Encoding.UTF8, "application/json");
+
+            // Act
+            var response = await Client.PostAsync("api/EnglishWords", content);
+            response.EnsureSuccessStatusCode();
+            
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            
+            // Assert
+
+        }
+    }
+}
